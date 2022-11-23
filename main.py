545650f32efe563f483361d8b075370ba035c370
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2022-11-23 14:36:21 +0300

    see2

--- main.py
+++ main.py
@@ -1,74 +1,59 @@
-# import numpy as np
-#
-# # n = int(input())
-# # rez = list(map(int, [input() for i in range(2 ** n)]))
-# # maska = []
-# # for i in range(2 ** n):
-# #     bet = [int(i) for i in str(bin(i))[2:]]
-# #     bet = [0] * (n - len(bet)) + bet
-# #     maska.append(bet)
-#
-#
-# def relu(x):
-#     return (x > 0) * x  # returns x if x > 0
-#     # return 0 otherwise
-#
-#
-# def relu2deriv(output):
-#     return output > 0  # returns 1 for input > 0
-#     # return 0 otherwise
-#
-#
-# alpha = 0.1 # * n
-#
-# hidden_size = 4
-# streetlights = np.array( [[ 1, 0, 1 ],
-#                           [ 0, 1, 1 ],
-#                           [ 0, 0, 1 ],
-#                           [ 1, 1, 1 ] ] )
-#
-# walk_vs_stop = np.array([[ 1, 1, 0, 0]]).T
-#
-# weights_0_1 = 2 * np.random.random((3, hidden_size)) - 1
-# weights_1_2 = 2 * np.random.random((hidden_size, 1)) - 1
-#
-# for iteration in range(1000):
-#     layer_2_error = 0
-#     for i in range(len(streetlights)):
-#         layer_0 = streetlights[i:i + 1]
-#         layer_1 = relu(np.dot(layer_0, weights_0_1))
-#         layer_2 = np.dot(layer_1, weights_1_2)
-#         layer_2_error += np.sum((layer_2 - walk_vs_stop[i:i + 1]) ** 2)
-#
-#         layer_2_delta = (walk_vs_stop[i:i + 1] - layer_2)
-#         layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
-#         weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-#         weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
-#         print(alpha * layer_1.T.dot(layer_2_delta))
-#         print(alpha * layer_0.T.dot(layer_1_delta))
-#
-#     print("Error:" + str(layer_2_error))
-#
-# print(weights_0_1, weights_1_2)
-# print(maska)
-# print(rez)
-# for i in range(int(input())):
-#     a = list(map(int, input().split()))
-#     layer_1 = relu(np.dot(a, weights_0_1))
-#     layer_2 = np.dot(layer_1, weights_1_2)
-#     print(round(layer_2[0]), layer_2)
-# # это решение не работает, поскольку формула имеет логический вид, но при домножении на коэффиценты она не сможет измениться
-import numpy as np
+# -*- coding: utf-8 -*-
+import math
+import random
+
+def dot1(first, second):
+    a = []
+    for j in range(len(first[0])):
+        b = []
+        for i in first:
+            b.append(i[j])
+        a.append(b)
+    first = a
+    a = []
+
+    for i in range(len(first)):
+        c = []
+        for f in range(len(second)):
+            b = []
+            k = 0
+            for j in second:
+                b.append(second[k][f] * first[i][k])
+                k += 1
+            c.append(sum(b))
+        a.append(c)
+    return a
+
+def persi(first, second):
+    b = []
+    for i in range(len(first)):
+        a = []
+        for j in range(len(first[0])):
+            a.append(first[i][j] * second[i][j])
+        b.append(a)
+    return b
+
+def dot(first, second):
+    a = []
+    for i in first:
+        d = []
+        for q, v in list(zip(i, second)):
+            d.append([q * g for g in v])
+        a.append([sum(h) for h in zip(*d)])
+    return a
 
 
-def nonlin(x, deriv=False):
+def nonlinmy(x, deriv=False):
     if (deriv == True):
-        return x * (1 - x)
+        return [[f * (1 - f) for f in pp] for pp in x]
+    return [[1 / (math.exp(-f) + 1) for f in pp] for pp in x]
 
-    return 1 / (1 + np.exp(-x))
-# ввод количества битовых симоволов от 0 до 10 включительно
+
 n = int(input())
-rez = list(map(lambda x: [int(x)], [input() for i in range(2 ** n)]))   # ввод значений при разных функциях тип при 0
+rez = []
+for i in range(2 ** n):
+    rez.append([int(input())])
+# ввод значений при разных функциях тип при 0
 # 0 одно, при 0 1 другое или тоже системе пофиг
 # Пример ввода:
 # 2
@@ -78,60 +63,43 @@ rez = list(map(lambda x: [int(x)], [input() for i in range(2 ** n)]))   # вво
 # 0
 # тип для 0 0 -> 0; 0 1 -> 1; 1 0 -> 1; 1 1 -> 0
 maska = []
-for i in range(2 ** n): #
+for i in range(2 ** n):
     bet = [int(i) for i in str(bin(i))[2:]]
     bet = [0] * (n - len(bet)) + bet
     maska.append(bet)
-X = np.array(maska)
-y = np.array(rez)
+x = maska
+y = rez
 hidses = 2 ** n
 vvod = n
 alfa = 0.5
 # случайно инициализируем веса, в среднем - 0
-syn0 = 2 * np.random.random((vvod, hidses)) - 1
-syn1 = 2 * np.random.random((hidses, 1)) - 1
-maink = [np.array([[1000]] * hidses), 0, 0]
+mysyn0 = [[2 * random.random() - 1 for j in range(hidses)] for i in range(vvod)]
+mysyn1 = [[2 * random.random() - 1] for i in range(hidses)]
 
-for j in range(60000):
+for j in range(6000):
 
     # проходим вперёд по слоям 0, 1 и 2
-    l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
-
+    myl0 = x
+    myl1 = nonlinmy(dot(myl0, mysyn0))
+    myl2 = nonlinmy(dot(myl1, mysyn1))
     # как сильно мы ошиблись относительно нужной величины
-    l2_error = y - l2
-
-    if (j % 10000) == 0:
-        print("Error:" + str(np.mean(np.abs(l2_error))))
-    if sum(l2_error) < sum(maink[0]):
-        maink = [l2_error, syn0, syn1]
-
+    l2_error_my = [[y[i][0] - myl2[i][0]] for i in range(len(myl2))]
     # в какую сторону нужно двигаться?
     # если мы были уверены в предсказании, то сильно менять его не надо
-    l2_delta = l2_error * nonlin(l2, deriv=True)
-
+    l2_delta_my = dot(l2_error_my, nonlinmy(myl2, deriv=True))
     # как сильно значения l1 влияют на ошибки в l2?
-    l1_error = l2_delta.dot(syn1.T)
-
-    # в каком направлении нужно двигаться, чтобы прийти к l1?
-    # если мы были уверены в предсказании, то сильно менять его не надо
-    l1_delta = l1_error * nonlin(l1, deriv=True)
-
-    syn1 += l1.T.dot(l2_delta) * alfa
-    syn0 += l0.T.dot(l1_delta) * alfa
-maska = []
-
-syn0 = maink[1]
-syn1 = maink[2]
-print(syn0)
-print(syn1)
-print(sum(abs(maink[0])) / 4)
+    l1_error_my = dot(l2_delta_my, [[i[0] for i in mysyn1]])
+    l1_delta_my = persi(l1_error_my, nonlinmy(myl1, deriv=True))
+    mysyn1 = [[round(i[0] + j[0], 7)] for i, j in list(zip(mysyn1, dot(myl1, [[alfa * i[0]] for i in l2_delta_my])))]
+    mysyn0 = [[i + j * alfa for i, j in list(zip(mysyn0[k], dot1(myl0, l1_delta_my)[k]))] for k in range(len(mysyn0))]
 # тут малясь системного вывода, а следом вывод всего и коэффициенты уверенности
 for i in range(2 ** vvod):
     bet = [int(i) for i in str(bin(i))[2:]]
     bet = [0] * (vvod - len(bet)) + bet
     l0 = bet
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
-    print(round(l2[0]), l2[0], l0)
+    myl0 = bet
+    myl1 = nonlinmy(dot([myl0], mysyn0))
+    myl2 = nonlinmy(dot(myl1, mysyn1))
+    print(round(myl2[0][0]), myl2[0][0])
+print(mysyn0)
+print(mysyn1)
--- solution50.py
+++ solution50.py
@@ -1,1 +1,36 @@
-print("YES")
+# def kontr(loc):
+#     su = 1
+#     for i in range(len(str(loc)) - 1):
+#         su *= int(str(loc)[i:i+2])
+#         if su == 0 or loc < su:
+#             return False
+#     if loc == su:
+#         return True
+#     return False
+#
+#
+# n = int(input())
+# for _ in range(n):
+#     l, r = map(int, input().split())
+#     cou = 0
+#     for i in range(max(10, l), r + 1):
+#         if kontr(i):
+#             cou += 1
+#     print(cou)
+import numpy as np
+
+# list1 = [[1,2,3,4], [5, 6, 7,8]]
+# list2 =  [[0.1,0.2, 0.3, 0.4],[0.5,0.6, 0.7, 0.8], [0.9, -0.1, -0.2, -0.3], [-0.4,-0.5, -0.6, -0.7]]
+list1 = [[0], [1]]
+list2 = [[0.1, 0.2], [0.3, 0.4]]
+vtr1 = np.array(list1)
+# output = [[2, 1], [5, 4]].[[3, 4], [7, 8]] = [[2*3+1*7, 2*4+1*8], [5*3+4*7, 5*4+4*8]] = [[13, 16], [43, 52]]
+vtr2 = np.array(list2)
+
+print("We create vector from a list 1:")
+print(vtr1)
+print("We create a vector from a list 2:")
+print(vtr2)
+
+vtr_product = vtr1.T.dot(vtr2)
+print("Dot product of two vectors: ", vtr_product)
