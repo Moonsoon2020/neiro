e5883d9d735be0c382036ff60f5788ed73a65fdf
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2022-11-24 10:12:38 +0300

    see5

--- solution73.py
+++ solution73.py
@@ -1,28 +1,24 @@
-n, k = map(int, input().split())
+n, u = map(int, input().split())
 x = list(map(int, input().split()))
 b = list(map(int, input().split()))
-# n, k = 10 ** 4 - 2, 1
-# x = '1' * (10 ** 4-3) + '2'
+# n, u = 10 ** 4 - 2, 10**4 - 2
+# x = [*range(1, 10**4)]
 # b = [*range(1, 10**4)]
 w = {}
-libs = {}
 facrez = 0
-bsum = sum(b) / len(b)
+bsum = sum(b) / n
 b1 = list(map(lambda alfa: bsum - alfa, b))
-bx = [i ** 2 for i in b1]
-sumbx = sum(bx)
-for k in x:
-    if k in w.keys():
-        facrez += w[k]
+sumbx = sum(map(lambda i: i ** 2, b1))
+for k in set(x):
+    asum = x.count(k) / n
+    a1 = list(map(lambda i: asum - int(i == k), x))
+    ax = sum(map(lambda i: i ** 2, a1))
+    ab = sum(map(lambda i: i[0] * i[1], zip(a1, b1)))
+    if ab == 0 or ax == 0:
+        w[k] = 0
     else:
-        a = [1 if i == k else 0 for i in x]
-        asum = sum(a) / len(a)
-        a1 = list(map(lambda alfa: asum - alfa, a))
-        ax = []
-        for i in a1:
-            ax.append(i ** 2)
-        ab = [i * j for i, j in zip(a1, b1)]
-        rez = sum(ab) / ((sum(ax) * sumbx) ** 0.5)
-        w[k] = rez
-        facrez += rez
+        w[k] = ab / ((ax * sumbx) ** 0.5)
+    # print(k)
+for k in x:
+    facrez += w[k]
 print(facrez / n)
